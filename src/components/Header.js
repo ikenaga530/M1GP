d6c322a189fd31d5d2606aa4833b06a7762a5f1e
ikenaga530 <takumi.ne.jp530@gmail.com>
2023-07-03 02:47:07 +0900

    second

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 const Header = () => {
   return (
     <header className="text-gray-700 border-b border-gray-200 ">
-      <div className="container flex mx-auto p-5">
+      <div className="container flex mx-auto p-3">
         <a href="#" className="font-medium text-gray-900">
           <span className="text-xl -ml-3">Map</span>
         </a>
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import {
+  CircleF,
+  GoogleMap,
+  LoadScript,
+  Marker,
+  MarkerF,
+} from "@react-google-maps/api";
 
 const Map = () => {
   const containerStyle = {
@@ -8,8 +14,8 @@ const Map = () => {
   };
 
   const center = {
-    lat: 35.69575,
-    lng: 139.77521,
+    lat: 34.688945692404246,
+    lng: 135.83986903087077,
   };
 
   const mapOptions = {
@@ -24,21 +30,63 @@ const Map = () => {
     ],
   };
 
+  const positionTodaiji = {
+    lat: 34.688945692404246,
+    lng: 135.83986903087077,
+  };
+
+  const positionNarakoen = {
+    lat: 34.68291679088236,
+    lng: 135.83231218845356,
+  };
+
+  const circleOptions = {
+    strokeColor: "#FF0000",
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: "#FF0000",
+    fillOpacity: 0.35,
+    clickable: false,
+    draggable: false,
+    editable: false,
+    visible: true,
+    radius: 30000,
+    zIndex: 1,
+  };
+
   return (
     <>
-      <div className="container px-5 py-20 mx-auto">
-        <div className="text-center mb-3">
+      <div className="container px-36 py-5 mx-auto">
+        <div className="text-center text-xl mb-3">
           <h1>混雑度可視化マップ</h1>
         </div>
-        <div>
+        <div className="">
           <LoadScript googleMapsApiKey={process.env.REACT_APP_MAP}>
             <GoogleMap
               mapContainerStyle={containerStyle}
               center={center}
-              zoom={17}
+              zoom={15}
               options={mapOptions} // オプションを指定する
-            ></GoogleMap>
+            >
+              <MarkerF position={positionTodaiji} />
+              <MarkerF position={positionNarakoen} />
+              <CircleF
+                center={positionTodaiji}
+                radius={50}
+                options={circleOptions}
+              />
+              <CircleF
+                center={positionNarakoen}
+                radius={100}
+                options={circleOptions}
+              />
+            </GoogleMap>
           </LoadScript>
+        </div>
+        <div className="flex flex-wrap">
+          <div className="md:w-1/3">
+            <div className="bg-gra-100">aaa</div>
+          </div>
         </div>
       </div>
     </>
